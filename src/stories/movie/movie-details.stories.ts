40b320574301afd3dc5e29d2df80b5b645c8e374
Andrii Shoferivskyi <Andrii_Shoferivskyi@epam.com>
2022-01-09 13:24:56 +0200

    vuex: add packeges + fix styles + add mocks + small refactor

--- src/stories/movie/movie-details.stories.ts
+++ src/stories/movie/movie-details.stories.ts
@@ -1,12 +1,12 @@
 import NrMovieDetails from '@/components/movie/MovieDetails.vue';
-import movie from '@/mock/movie';
+import movies from '@/mock/movies';
 
 export default { title: 'MovieDetails' };
 
 export const movieDetails = (): unknown => ({
   components: { NrMovieDetails },
   computed: {
-    args: () => ({ movie }),
+    args: () => ({ movie: movies[0] }),
   },
   template: '<nr-movie-details v-bind="args"></nr-movie-details>',
 });
