9261e7aded295fcc4f7e8042e80cbce5745157a8
Andrii Shoferivskyi <Andrii_Shoferivskyi@epam.com>
2022-01-28 14:18:27 +0200

    e2e cypress tests

---
+++ tests/e2e/specs/details.ts
@@ -0,0 +1,20 @@
+describe('Details', () => {
+  it('should render 10 suggested movies', () => {
+    cy.visit('/#/details/447365');
+    cy.get('.movies__item').should('have.length', 10);
+    cy.get('.movies__highlight').should('be', '10');
+  });
+
+  it('should render movie details', () => {
+    cy.visit('/#/details/447365');
+    cy.get('.movie-details__title')
+      .first()
+      .should('be', 'Guardians of the Galaxy Vol. 3');
+    cy.get('.movie-details__rating').first().should('be', '0');
+    cy.get('.movie-details__genre')
+      .first()
+      .should('be', 'Action, Adventure, Science Fiction');
+    cy.get('.movie-details__date').first().should('be', '2020');
+    cy.get('.movie-details__duration').first().should('be', '0 min');
+  });
+});
---
+++ tests/e2e/specs/home.ts
@@ -0,0 +1,22 @@
+describe('Home', () => {
+  it('should render 10 movies', () => {
+    cy.visit('/');
+    cy.get('.movies__item').should('have.length', 10);
+    cy.get('.movies__highlight').should('be', '10');
+  });
+
+  it('should search for film', () => {
+    cy.visit('/');
+    cy.get('.search .input__field').type('Transformers 7');
+    cy.wait(200);
+    cy.get('.search .button').click();
+    cy.get('.movies__item').should('have.length', 1);
+  });
+
+  it('should redirect to details', () => {
+    cy.visit('/');
+    cy.get('.movie-card__link').first().click();
+    cy.wait(200);
+    cy.location('hash').should('eq', '#/details/447365');
+  });
+});
